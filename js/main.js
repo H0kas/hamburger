a0c9fc7d07b14788f02f7f5eab926ae1dd9636e4
Sebastian <34491851+H0kas@users.noreply.github.com>
2018-01-11 15:17:21 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function () {
-    document.querySelector(".hamburger").classList.toggle("open");
+    document.querySelector(".hamburger").classList.toggle("cross");
+}, false);
+
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+    document.querySelector(".menu").classList.toggle("open");
 }, false);
